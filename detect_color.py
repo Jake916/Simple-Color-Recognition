4c66539cd123f6296de59bd23792c17b4451bade
Jacob Prouse <jprouse2@uwo.ca>
2018-01-20 22:19:03 -0500

    Added python script for color recognition
    -Script for color recognition

---
+++ detect_color.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import argparse
+
+#construct argument
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
+
+#load image
+image = cv2.imread(args["image"])
+
+#list of boundaries
+boundaries = [
+    ([17,15,100],[50,56,250]),
+    ([86,31,4],[220,88,50])
+]
+
+#loop over the boundaries
+for (lower, upper) in boundaries:
+    #create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype = "uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    # show the images
+    cv2.imshow("images", np.hstack([image, output]))
+    cv2.waitKey(0)
